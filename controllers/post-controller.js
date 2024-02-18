1a8f38a6ff38a542cc50e79bb727cdb28eaa1412
tamir.rust <timorust1@gmail.com>
2024-02-18 18:44:05 +0200

    create post router

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -2,7 +2,28 @@ const { prisma } = require('../prisma/prisma-client')
 
 const PostController = {
 	createPost: async (req, res) => {
-		res.send('createPost')
+		const { content } = req.body
+
+		const authorId = req.user.userId
+
+		if (!content) {
+			return res.status(400).json({ error: ' All fields are required' })
+		}
+
+		try {
+			const post = await prisma.post.create({
+				data: {
+					content,
+					authorId,
+				},
+			})
+
+			res.json(post)
+		} catch (error) {
+			console.error('Create post error', error)
+
+			res.status(500).json({ error: ' Internal server error' })
+		}
 	},
 	getAllPosts: async (req, res) => {
 		res.send('getAllPosts')
