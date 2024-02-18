40eaebf6982316716a85587f27b6c61c9ba83d9e
tamir.rust <timorust1@gmail.com>
2024-02-19 01:19:03 +0200

    createComment

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -25,7 +25,29 @@ const CommentController = {
 		}
 	},
 	deleteComment: async (req, res) => {
-		res.send('deleteComment')
+		const { id } = req.params
+		const userId = req.user.userId
+
+		try {
+			const comment = await prisma.comment.findUnique({
+				where: { id },
+			})
+
+			if (!comment) return res.status(404).json({ error: 'Comment not found' })
+
+			if (comment.userId !== userId)
+				return res.status(403).json({ error: ' No access' })
+
+			await prisma.comment.delete({
+				where: { id },
+			})
+
+			res.json(comment)
+		} catch (error) {
+			console.error('Error deleting comment', error)
+
+			res.status(500).json({ error: 'Internal server error' })
+		}
 	},
 }
 
