708cfa37c717a124dc7b4690e883123797a623a1
tamir.rust <timorust1@gmail.com>
2024-02-18 17:12:48 +0200

    current and finish user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -153,7 +153,37 @@ const UserController = {
 		}
 	},
 	current: async (req, res) => {
-		res.send('current')
+		try {
+			const user = await prisma.user.findUnique({
+				where: {
+					id: req.user.userId,
+				},
+				include: {
+					followers: {
+						include: {
+							follower: true,
+						},
+					},
+					following: {
+						include: {
+							following: true,
+						},
+					},
+				},
+			})
+
+			if (!user) {
+				return res.status(400).json({
+					error: 'User not found',
+				})
+			}
+
+			res.json(user)
+		} catch (error) {
+			console.error(' Get Current Error', error)
+
+			res.status(500).json({ error: 'Internal server error' })
+		}
 	},
 }
 
