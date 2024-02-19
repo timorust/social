8e4367f9d434ce6b6e4920922ac38e5cd3b42179
tamir.rust <timorust1@gmail.com>
2024-02-19 10:44:28 +0200

    likePost router

--- controllers/like-controller.js
+++ controllers/like-controller.js
@@ -2,7 +2,37 @@ const { prisma } = require('../prisma/prisma-client')
 
 const LikeController = {
 	likePost: async (req, res) => {
-		res.send('likePost')
+		const { postId } = req.body
+
+		const userId = req.user.userId
+
+		if (!postId)
+			return res.status(400).json({ error: ' All fields are required' })
+
+		try {
+			const existingLike = await prisma.like.findFirst({
+				where: {
+					postId,
+					userId,
+				},
+			})
+
+			if (existingLike)
+				return res.status(400).json({ error: 'You already liked it' })
+
+			const like = await prisma.like.create({
+				data: {
+					postId,
+					userId,
+				},
+			})
+
+			res.json(like)
+		} catch (error) {
+			console.error('Error like post', error)
+
+			res.status(500).json({ error: 'Internal server error' })
+		}
 	},
 
 	unLikePost: async (req, res) => {
