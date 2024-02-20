398f00b9aa5a187cbced48e27f8b31327982688e
tamir.rust <timorust1@gmail.com>
2024-02-20 07:19:18 +0200

    followUser & unFollowUser routers

--- controllers/follow-controller.js
+++ controllers/follow-controller.js
@@ -2,11 +2,67 @@ const { prisma } = require('../prisma/prisma-client')
 
 const FollowController = {
 	followUser: async (req, res) => {
-		res.send('followUser')
+		const { followingId } = req.body
+
+		const userId = req.user.userId
+
+		if (followingId === userId)
+			return res.status(500).json({ error: "You can't follow yourself" })
+
+		try {
+			const existingSubscription = await prisma.follows.findFirst({
+				where: {
+					AND: [{ followerId: userId }, { followingId: followingId }],
+				},
+			})
+
+			if (existingSubscription)
+				return res.status(400).json({ error: 'Subscription already exists' })
+
+			await prisma.follows.create({
+				data: {
+					followerId: userId,
+					followingId: followingId,
+				},
+			})
+
+			res.status(201).json({ message: 'Successfully created subscription' })
+		} catch (error) {
+			console.error('Follow failed', error)
+
+			res.status(500).json({ error: 'Internal server error' })
+		}
 	},
 
 	unFollowUser: async (req, res) => {
-		res.send('unFollowUser')
+		const { followingId } = req.body
+
+		const userId = req.user.userId
+
+		try {
+			const follows = await prisma.follows.findFirst({
+				where: {
+					AND: [{ followerId: userId }, { followingId }],
+				},
+			})
+
+			if (!follows)
+				return res
+					.status(404)
+					.json({ error: 'You are not following this user' })
+
+			await prisma.follows.delete({
+				where: {
+					id: follows.id,
+				},
+			})
+
+			res.status(201).json({ message: 'You unsubscribed' })
+		} catch (error) {
+			console.error('Unfollow failed', error)
+
+			res.status(500).json({ error: 'Internal server error' })
+		}
 	},
 }
 
