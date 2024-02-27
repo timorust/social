b7163a8924291b5865b16f86cc169e5070f7e480
tamir.rust <timorust1@gmail.com>
2024-02-27 15:02:22 +0200

    cors

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const fs = require('fs')
+const cors = require('cors')
 
 const app = express()
 // view engine setup
-
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
